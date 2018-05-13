f532ea46da251d052cd59a07b92e1f264d359e80
Administrator <Administrator@ICOS-20170908YH>
2018-05-13 22:17:40 +0800

    init

--- src/main/java/shopping/controller/TestController.java
+++ src/main/java/shopping/controller/TestController.java
@@ -4,7 +4,6 @@ import shopping.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import shopping.service.TestService;
