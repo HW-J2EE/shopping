c95154eddf80e0abe7d589b4ea15997f6bbe2d1c
SupermanCj <810121619@qq.com>
2018-05-16 18:52:57 +0800

    cart completed

--- src/main/java/shopping/controller/CartController.java
+++ src/main/java/shopping/controller/CartController.java
@@ -1,18 +1,9 @@
 package shopping.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import shopping.model.CartDetail;
