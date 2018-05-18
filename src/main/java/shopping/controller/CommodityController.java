340a9ec4f87e5c620328d29f06d4c090281ba83f
SupermanCj <810121619@qq.com>
2018-05-18 11:51:19 +0800

    review

--- src/main/java/shopping/controller/CommodityController.java
+++ src/main/java/shopping/controller/CommodityController.java
@@ -4,10 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
--- src/main/java/shopping/service/impl/CommodityServiceImpl.java
+++ src/main/java/shopping/service/impl/CommodityServiceImpl.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
-import javax.servlet.http.Part;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
